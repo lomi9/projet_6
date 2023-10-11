0c0428e173371487ffa6e0f8ce35b7203105bd1e
Loriane MICHEL <loriane.michel94@gmail.com>
2023-10-11 16:56:20 +0200

    améliorations scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/main.scss";
+import "./styles/reset.scss";
+import "./styles/utilities.scss";
 import router from "./pages/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
